ad37daa08d0e0640f4167b6f3abfc12d201b7c96
NguyenQuyDat <nguyenquydat.8121@gmail.com>
2021-09-10 18:53:32 +0200

    - UPDATE: remove save image thread

--- config.py
+++ config.py
@@ -3,6 +3,4 @@ class Config(object):
     PORT = 5000
     LOG_FILE_PATH = ""
     NR_DISPLAY_LOG_LINES = 30
-    SAVE_IMAGE_TIME_OFFSET = 2
     IMAGE_FILE_PATH = ""
-    IMAGE_SAVE_PATH = ""
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
 from flask import Flask
 
 import config
 from app.api import debug
-import utils
 
 
 app = Flask(__name__, static_url_path="", static_folder="./template", template_folder="./template")
@@ -12,6 +9,4 @@ app.register_blueprint(debug.api, url_prefix='/')
 
 
 if __name__ == '__main__':
-    thread = threading.Thread(target=utils.save_image_from_camera)
-    thread.start()
     app.run(host=config.Config.HOST, port=config.Config.PORT, threaded=True, use_reloader=True, debug=True)
